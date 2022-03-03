8b16fb1abc4c6706f368e8ddb41b469feee277c6
vectorPY <vector.thedev@gmail.com>
2022-03-03 18:00:40 +0100

    Format login form

--- auth/login_form.php
+++ auth/login_form.php
@@ -25,65 +25,51 @@
     <!-- Bootstrap core CSS -->
     <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet" />
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
-    <link href="signin.css" rel="stylesheet" />
+    <?php 
+        include_once "../static/header.php";
+    ?>
   </head>
+
   <body class="text-center">
-    <main class="form-signin">
-      <form action="login.php" method="post">
-        <img
-          class="mb-4"
-          src="../assets/brand/bootstrap-logo.svg"
-          alt=""
-          width="72"
-          height="57"
-        />
-        <h1 class="h3 mb-3 fw-normal">Bitte melden Sie sich an!</h1>
+    <div class="row justify-content-center">
+      <div class="col-md-4 col-md-offset-5 align-self-center">
+        <form action="login.php" method="post">
+          <h1 class="h3 mb-3 fw-normal">Bitte melden Sie sich an!</h1>
 
-        <div class="form-floating">
+          <div class="form-group">
+            <input
+              type="text"
+              class="form-control"
+              name="username"
+              placeholder="Username"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="password"
+              class="form-control"
+              name="passwort"
+              placeholder="Passwort"
+            />
+          </div>
           <input
-            type="text"
-            class="form-control"
-            name="username"
-            placeholder="Username"
+            class="w-100 btn btn-lg btn-primary"
+            type="submit"
+            name="submit"
+            value="Einloggen"
           />
-        </div>
-        <div class="form-floating">
-          <input
-            type="password"
-            class="form-control"
-            name="passwort"
-            placeholder="Passwort"
-          />
-        </div>
-        <input
-          class="w-100 btn btn-lg btn-primary"
-          type="submit"
-          name="submit"
-          value="Einloggen"
-        />
 
-        <p class="mt-5 mb-3 text-muted">&copy; 2022</p>
+          <p class="mt-5 mb-3 text-muted">&copy; 2022</p>
 
-        <br />
-        <a href="register_form.php">Haben Sie noch keinen Account?</a>
-        <br />
-      </form>
-    </main>
+          <br />
+          <a href="register_form.php">Haben Sie noch keinen Account?</a>
+          <br />
+        </form>
+      </div>
+    </div>
+
+  <?php 
+      include_once "../static/footer.php";
+  ?>
   </body>
 </html>
