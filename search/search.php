0c075ba9a310ace5edb830c2247fb5389768aed8
vectorPY <vector.thedev@gmail.com>
2022-03-17 09:29:17 +0100

    simple search

--- search/search.php
+++ search/search.php
@@ -1,6 +1,16 @@
 <?php 
 
+include "../inc.php";
+include "../utils/article.php";
+
+$con = mysqli_connect($host, $user, $passwd, $datenbank) or die("Die Datenbank ist momentan nicht erreichbar!");
+
 $search = $_POST["search"];
-echo $search;
+
+$res = search_article($con, $search);
+
+$rows = mysqli_fetch_all($res);
+
+print_r($rows);
 
 ?>
