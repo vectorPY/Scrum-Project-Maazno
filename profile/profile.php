a7bab572ff552805124f9cd15f581530d0555558
vectorPY <vector.thedev@gmail.com>
2022-03-17 08:48:01 +0100

    improved profile page

--- profile/profile.php
+++ profile/profile.php
@@ -1,73 +1,99 @@
-<?php
+<html>
+    <head>
+        <title>
+            Profilübersicht
+        </title>
+    </head>
+</html>
 
-include "../inc.php";
-$con = mysqli_connect($host,$user,$passwd,$datenbank) or die("Die Datenbank ist momentan nicht erreichbar!");
+<?php
 
 session_start();
 
-$user = 1;
-$query="select * from nutzer,ort,land where nutzer.ort_id=ort.ort_id and ort.land_id=land.land_id and nutzer_id=$user;";     #sql abfrage mit suche nach dem user      
-$result=mysqli_query($con,$query);
-if($result->num_rows == 0){
-    echo "es gibt keine ergebnisse";
-    header("location:link.html");
-}else{  
-    $r = $result->fetch_assoc();
-    $row=array_values($r);
-print_r ($row);
-    echo "
-    <table >
-    <thead>
-        <tr>
-            <th>Deine Profilübersicht</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Nachname:</td>
-            <td> $row[1]</td>
-        </tr>
-        <tr>
-            <td>Vorname:</td>
-            <td> $row[2]</td>
-        </tr>
-        <tr>
-            <td>Telefonnummer:</td>
-            <td> $row[3]</td>
-        </tr>
-        <tr>
-            <td>Email:</td>
-            <td> $row[8]</td>
-        </tr>
-        <tr>
-            <td>Straße:</td>
-            <td> $row[4]</td>
-        </tr>
-        <tr>
-            <td>Hausnummer:</td>
-            <td> $row[5]</td>
-        </tr>
-        <tr>
-            <td>Passwort:</td>
-            <td> $row[6]</td>
-        </tr>
+// redirect zur index Seite, wenn nicht eingeloggt
+if (empty($_SESSION["nutzer_id"])) {
+    header("Location: ../index.php");
+}
 
-        <tr>
-            <td>Ort:</td>
-            <td> $row[9]</td>
-        </tr>
-        <tr>
-            <td>PLZ:</td>
-            <td> $row[10]</td>
-        </tr>
-        <tr>
-            <td>Land:</td>
-            <td> $row[12]</td>
-        </tr>
-        </tbody>
-    </table>>
+include_once "../static/header.php";
+
+echo "<center><h1>Ihr Profil</h1></center><br>";
+
+include "../inc.php";
+include "../utils/alert.php";
+
+$con = mysqli_connect($host, $user, $passwd, $datenbank) or die("Die Datenbank ist momentan nicht erreichbar!");
+
+// Nutzer - Id aus der Session entnehmen
+$user = $_SESSION["nutzer_id"];
+
+// Nutzerdaten abfragen
+$query = "SELECT * FROM nutzer, ort, land WHERE nutzer.ort_id=ort.ort_id AND ort.land_id=land.land_id AND nutzer_id=$user;";         
+$result = mysqli_query($con, $query);
+
+if ($result->num_rows == 0) {
+    php_alert( "Es gibt keine Ergebnisse" );
+    header("Refresh: 1; URL=" . $base_url . "../index.html");
+} else {  
+    // Werte werden in einem Array gespeichert
+    $r = $result->fetch_assoc();
+    $row = array_values($r);
+    
+    // Tabelle ausgeben
+    echo "
+        <div class='row justify-content-center'>
+            <div class='col-auto'>
+                <table border=1 class='table table-dark table-hover'>
+                <thead class='thead-dark'>
+                    <tr>
+                        <th>Attribut</th>
+                        <th>Wert</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Nachname:</td>
+                        <td> $row[1]</td>
+                    </tr>
+                    <tr>
+                        <td>Vorname:</td>
+                        <td> $row[2]</td>
+                    </tr>
+                    <tr>
+                        <td>Telefonnummer:</td>
+                        <td> $row[3]</td>
+                    </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td> $row[8]</td>
+                    </tr>
+                    <tr>
+                        <td>Straße:</td>
+                        <td> $row[4]</td>
+                    </tr>
+                    <tr>
+                        <td>Hausnummer:</td>
+                        <td> $row[5]</td>
+                    </tr>
+                    <tr>
+                        <td>Ort:</td>
+                        <td> $row[9]</td>
+                    </tr>
+                    <tr>
+                        <td>PLZ:</td>
+                        <td> $row[10]</td>
+                    </tr>
+                    <tr>
+                        <td>Land:</td>
+                        <td> $row[13]</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     ";
 } 
-       
-       
+
+include_once "../static/footer.php";
+
 ?>
---
+++ utils/alert.php
@@ -0,0 +1,12 @@
+<?php 
+
+// der Ordner in htdocs muss entsprechend benannt werden
+$base_url = "http://localhost/Scrum-Project-Maazno";
+
+// ruft eine alert Box auf
+// basiert auf: https://stackoverflow.com/questions/13837375/how-to-show-an-alert-box-in-php
+function php_alert($msg) {
+    echo '<script type="text/javascript">alert("' . $msg . '")</script>';
+}
+ 
+?>
