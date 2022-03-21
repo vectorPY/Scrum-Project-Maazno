c4a0e82da9456e0616fc55721cc73b38ac7eb19e
Jaromir-Pospisil <jaromir.pospisil@gmx.degit config --global --replace-all  user.email jaromir.pospisil@gmx.degit config --global --replace-all  user.email jaromir.pospisil@gmx.de>
2022-03-21 09:48:57 +0100

    Hinzufuegen von Kommentaren

--- cart/add_to_cart.php
+++ cart/add_to_cart.php
@@ -2,10 +2,12 @@
     include_once("../inc.php");
     include_once("../utils/cart.php");
 
+    
+    // Informationen aus POST herausnehmen 
     $anzahl= $_POST["menge"];
     $artikel_id = $_POST["artikel_id"];
     $nutzer_id = $_POST["nutzer_id"];
     
-    add_to_cart($con, $artikel_id, $anzahl, $nutzer_id);
-    header("Location: ../index.php");
-    exit;
+    add_to_cart($con, $artikel_id, $anzahl, $nutzer_id); //Artikel in den Warenkorb hinzufuegen
+    header("Location: ../index.php"); //Umleiten zur homepage
+    exit; //Beenden des Codes
