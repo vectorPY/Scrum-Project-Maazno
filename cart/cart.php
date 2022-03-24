e3be9b468792ed006be8ca559873f3e66faf622a
vectorPY <vector.thedev@gmail.com>
2022-03-24 15:58:53 +0100

    Revert "Update cart.php"
    This reverts commit 5f05611c12494f80e8ab3dc4fb61192173a82b82.

--- cart/cart.php
+++ cart/cart.php
@@ -1,19 +1,19 @@
-<<?php
+<?php
 session_start();
 
 include("../inc.php");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $con = mysqli_connect($host,$user,$passwd,$datenbank);
-    $id = $_POST['warenkorb_id'];
-    $anzahl = $_POST['anzahl'];
-    $userId = $_POST['nutzer_id'];
+    $id = $_POST['id'];
+    $qty = $_POST['qty'];
+    $userId = $_POST['user_id'];
     if ($userId > 0){
-        $sql = "INSERT INTO  im_warenkorb(artikel_id,nutzer_id,anzahl) VALUES('$id','$userId','$anzahl')";
+        $sql = "INSERT INTO  cart(product_id,user_id,qty) VALUES('$id','$userId','$qty')";
         $result = mysqli_query($con, $sql);
         $html = '';
         if ($result){
-            $newSql = "SELECT artikel.bild as bild,artikel.name as name,im_warenkorb.anzahl as anzahl,artikel.preis as preis  FROM im_warenkorb INNER  JOIN artikel ON im_warenkorb.artikel_id = artikel.artikel_id WHERE im_warenkorb.nutzer_id = '$userId' ORDER  BY  im_warenkorb.created_at DESC";
+            $newSql = "SELECT artikel.bild as bild,artikel.name as name,cart.qty as qty,artikel.preis as preis  FROM cart INNER  JOIN artikel ON cart.product_id = artikel.artikel_id WHERE cart.user_id = '$userId' ORDER  BY  cart.created_at DESC";
             $rs = mysqli_query($con, $newSql);
             if (mysqli_num_rows($rs) > 0){
                 while($row = mysqli_fetch_assoc($rs)){
@@ -22,8 +22,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
                                 <img style="max-width: 60px;float: left" class="align-self-center mr-3" src="data:image/jpg/png/;charset=utf8;base64,'.base64_encode($row['bild']).'" alt="Generic placeholder image">
                                 <div class="media-body">
                                     <h5 class="mt-0">'.$row['name'].'</h5>
-                                    <span class="d-block">Quantity: '.$row['anzahl'].'</span>
-                                    <span class="d-block">Price: '.((int) $row['anzahl'] * (int)$row['preis']).'</span>
+                                    <span class="d-block">Quantity: '.$row['qty'].'</span>
+                                    <span class="d-block">Price: '.((int) $row['qty'] * (int)$row['preis']).'</span>
                                 </div>
                             </div>
                         </li><hr>';
